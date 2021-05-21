f2c73592f5a94a924c3abafc1373d34698d1732b
Pera030 <pericapaunovic@gmail.com>
2021-05-21 23:31:54 +0200

    prva promena

---
+++ index.php
@@ -0,0 +1,430 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Medex</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <!-- Top Bar -->
+    <section class="topBar">
+        <div class="container">
+            <div class="col">
+                <p><i class="fas fa-paper-plane"></i> youremail@email.com</p>
+                <div class="icons">
+                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                    <a href=""><i class="fab fa-twitter"></i></a>
+                    <a href=""><i class="fab fa-instagram"></i></a>
+                    <a href=""><i class="fas fa-basketball-ball"></i></a>
+                </div>
+            </div>
+            <div class="col">
+                <button type="button">Book An Appointment</button>
+            </div>
+        </div>
+    </section>
+    
+    <!-- Nav Top-->
+    <section class="container navTop">
+        <div class="col">
+            <h3><a href="index.php"><i class="fas fa-clinic-medical"></i>MEDEX</a></h3>
+        </div>
+        <div class="col">
+            <i class="far fa-map"></i>
+            <div class="text">
+                <h5><span>Free call</span> +1 234 456 78910</h5>
+                <p>Call Us Now 24/7 Customer Support</p>
+            </div>
+        </div>
+        <div class="col">
+            <i class="far fa-paper-plane"></i>
+            <div class="text">
+                <h5>Our Location</h5>
+                <p>198 West 21th Street, Suite 721 New York NY 10016</p>
+            </div>
+        </div>
+    </section>
+    
+    <!-- Navigation -->
+    <nav>
+        <div class="container">
+            <ul>
+                <li><a href="index.php" class="active">Home</a></li>
+                <li><a href="index.php">About</a></li>
+                <li><a href="index.php">Services</a></li>
+                <li><a href="index.php">Doctors</a></li>
+                <li><a href="index.php">Departments</a></li>
+                <li><a href="index.php">Pricing</a></li>
+                <li><a href="index.php">Gallery</a></li>
+                <li><a href="index.php">Blog</a></li>
+                <li><a href="index.php">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+    
+    <!-- Header -->
+    <header>
+       <div class="container">
+            <h1>The Most Valuable Thing is Your Health</h1>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.</p>
+            <form>
+                <select>
+                    <option value="0">Select Department</option>
+                    <option value="1">Neurology</option>
+                    <option value="2">Cardiology</option>
+                    <option value="3">X-ray</option>
+                    <option value="4">Dental</option>
+                    <option value="5">Oftamology</option>
+                    <option value="6">Other Services</option>
+                </select>
+                <i class="fas fa-angle-down"></i>
+                <input type="submit" value="Book Appointment">
+            </form>
+        </div>
+    </header>
+    
+    <!-- Welcome -->
+    
+    <section class="welcome">
+        <div class="container">
+            <article>
+                <h2>Welcome to <span>Medex</span></h2>
+                <div class="row">
+                    <div class="col">
+                        <i class="fas fa-ambulance"></i>
+                        <div class="text">
+                            <h4>Emergency help</h4>
+                            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <i class="fas fa-user-md"></i>
+                        <div class="text">
+                            <h4>Qualified Doctors</h4>
+                            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <i class="fas fa-flag"></i>
+                        <div class="text">
+                            <h4>Location &amp; Directions</h4>
+                            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <i class="fas fa-stethoscope"></i>
+                        <div class="text">
+                            <h4>Medical Treatment</h4>
+                            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <aside>
+                <h3>Appointemnt form</h3>
+                <form>
+                    <input type="text" placeholder="First Name">
+                    <input type="text" placeholder="Last Name">
+                    <select>
+                        <option value="0">Select your services</option>
+                    </select>
+                    <i class="fas fa-angle-down"></i>
+                    <input type="text" placeholder="Phone">
+                    <input type="text" placeholder="Date">
+                    <input type="text" placeholder="Time">
+                    <textarea cols="30" rows="10" placeholder="Message"></textarea>
+                    <input type="submit" value="Appointment">
+                </form>
+            </aside>
+        </div>
+    </section>
+    
+    <!-- About -->
+    <section class="about container">
+        <div class="col">
+            <img src="img/about.jpg" alt="">
+        </div>
+        <div class="col">
+            <h2>We are <span>Medex</span> a healthcare provider</h2>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+            <div class="buttons">
+                <button type="button">Make an appointment</button>
+                <input type="submit" value="Contact us">
+            </div>
+        </div>
+    </section>
+    
+    <!-- Advertisment -->
+    <section class="advertisment">
+        <div class="container">
+            <h2>Your Health is Our Priority</h2>
+            <p>We can manage your dream building A small river named Duden flows by their place</p>
+            <button type="button">Make an Appointment</button>
+        </div>
+    </section>
+    
+    <!-- Departments -->
+    <section class="departments container">
+        <h2>Medex <span>Department</span></h2>
+        <div class="row">
+            <div class="col">
+                <a href=""><i class="fas fa-clinic-medical"></i> Neurology</a>
+                <a href=""><i class="fas fa-clinic-medical"></i> Ophtalmology</a>
+                <a href=""><i class="fas fa-clinic-medical"></i> Nuclear Magnetic</a>
+                <a href=""><i class="fas fa-clinic-medical"></i> X-ray</a>
+                <a href=""><i class="fas fa-clinic-medical"></i> Surgical</a>
+                <a href=""><i class="fas fa-clinic-medical"></i> Cardiology</a>
+                <a href=""><i class="fas fa-clinic-medical"></i> Dental Clinic</a>
+            </div>
+            <div class="col">
+                <img src="img/dept-1.jpg" alt="">
+            </div>
+            <div class="col">
+                <h3>Neurology</h3>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                <p>The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
+                <ul>
+                    <li><i class="fas fa-check"></i> The Big Oxmox advised her not to do so</li>
+                    <li><i class="fas fa-check"></i> Far far away, behind the word mountains</li>
+                    <li><i class="fas fa-check"></i> Separated they live in Bookmarksgrove</li>
+                    <li><i class="fas fa-check"></i> She packed her seven versalia</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    
+    <!-- Team -->
+    
+    <section class="team">
+       <h2>Our Qualified Doctors</h2>
+        <div class="row">
+            <div class="col">
+                <img src="img/doc-1.jpg" alt="">
+                <div class="text">
+                    <h4>Dr. Lloyd Wilson</h4>
+                    <h5>Neurologist</h5>
+                    <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-google"></i>
+                    <i class="fab fa-instagram"></i>
+                </div>
+            </div>
+            <div class="col">
+                <img src="img/doc-2.jpg" alt="">
+                <div class="text">
+                    <h4>Dr. Rachel Parker</h4>
+                    <h5>Ophthalmologist</h5>
+                    <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-google"></i>
+                    <i class="fab fa-instagram"></i>
+                </div>
+            </div>
+            <div class="col">
+                <img src="img/doc-3.jpg" alt="">
+                <div class="text">
+                    <h4>Dr. Ian Smith</h4>
+                    <h5>Dentist</h5>
+                    <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-google"></i>
+                    <i class="fab fa-instagram"></i>
+                </div>
+            </div>
+            <div class="col">
+                <img src="img/doc-4.jpg" alt="">
+                <div class="text">
+                    <h4>Dr. Alicia Henderson</h4>
+                    <h5>Pediatrician</h5>
+                    <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-google"></i>
+                    <i class="fab fa-instagram"></i>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+     <!-- Advertisment1 -->
+    <section class="advertisment1">
+        <div class="container">
+            <div class="col">
+                <h4>30</h4>
+                <h5>YEARS OF EXPERIENCED</h5>
+            </div>
+            <div class="col">
+                <h4>4,500</h4>
+                <h5>HAPPY PATIENTS</h5>
+            </div>
+            <div class="col">
+                <h4>84</h4>
+                <h5>NUMBER OF DOCTORS</h5>
+            </div>
+            <div class="col">
+                <h4>300</h4>
+                <h5>NUMBER OF STAFFS</h5>
+            </div>
+        </div>
+    </section>
+    
+    <!-- Blog -->
+    <section class="blog">
+        <h2>Latest Blog Updates</h2>
+        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+        <div class="row">
+            <div class="col">
+                <img src="img/image_1.jpg" alt="">
+                <div class="data">
+                    <div class="links">
+                        <a href=""><i class="fas fa-gift"></i> AUG. 12.<br>2020</a><br>
+                        <a href=""><i class="fas fa-user"></i> ADMIN</a><br>
+                        <a href=""><i class="fas fa-comment"></i> 3</a>
+                    </div>
+                    <div class="text">
+                        <h3>Scary Thing That You Don’t Get Enough Sleep</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia...</p>
+                        <input type="submit" value="Read more">
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <img src="img/image_2.jpg" alt="">
+                <div class="data">
+                    <div class="links">
+                        <a href=""><i class="fas fa-gift"></i> AUG. 12.<br>2020</a><br>
+                        <a href=""><i class="fas fa-user"></i> ADMIN</a><br>
+                        <a href=""><i class="fas fa-comment"></i> 3</a>
+                    </div>
+                    <div class="text">
+                        <h3>Scary Thing That You Don’t Get Enough Sleep</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia...</p>
+                        <input type="submit" value="Read more">
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <img src="img/image_3.jpg" alt="">
+                <div class="data">
+                    <div class="links">
+                        <a href=""><i class="fas fa-gift"></i> AUG. 12.<br>2020</a><br>
+                        <a href=""><i class="fas fa-user"></i> ADMIN</a><br>
+                        <a href=""><i class="fas fa-comment"></i> 3</a>
+                    </div>
+                    <div class="text">
+                        <h3>Scary Thing That You Don’t Get Enough Sleep</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia...</p>
+                        <input type="submit" value="Read more">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!-- Testemonials -->
+    
+    <section class="testimonials">
+        <h5>Read testimonials</h5>
+        <h2>Our Patient Says</h2>
+        <div class="container">
+            <div class="col">
+                <img src="img/person_1.jpg" alt="">
+                <i class="fas fa-quote-left"></i>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+                    <h5>Jeff Freshman</h5>
+                    <h4>PATIENTS</h4>
+                </div>
+            </div>    
+            <div class="col">
+                <img src="img/person_2.jpg" alt="">
+                <i class="fas fa-quote-left"></i>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+                    <h5>Jeff Freshman</h5>
+                    <h4>PATIENTS</h4>
+                </div>
+            </div>   
+            <div class="col">
+                <img src="img/person_3.jpg" alt="">
+                <i class="fas fa-quote-left"></i>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+                    <h5>Jeff Freshman</h5>
+                    <h4>PATIENTS</h4>
+                </div>
+            </div>   
+        </div>
+    </section>
+    
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="col">
+                <h4><a href="index.php"><i class="fas fa-clinic-medical"></i>MEDEX</a></h4>
+                <p>Far far away, behind the word mountains, far from the countries.</p>
+                <a href=""><i class="fab fa-twitter"></i></a>
+                <a href=""><i class="fab fa-facebook-f"></i></a>
+                <a href=""><i class="fab fa-instagram"></i></a>
+            </div>
+            <div class="col">
+                <h4>DEPARTMENTS</h4>
+                <ul>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Neurology</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Ophthalmology</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Nuclear Magnetic</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">X-Ray</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Surgical</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Cardiology</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Dental</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4>LINKS</h4>
+                <ul>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Home</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Ophthalmology</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">About</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Departments</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Doctors</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Blog</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Pricing</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4>SERVICES</h4>
+                <ul>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Emergency Help</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Qualified Doctors</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Location &amp; Directions</a></li>
+                    <li><i class="fas fa-chevron-right"></i> <a href="">Medical Treatments</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4>HAVE A QUESTIONS</h4>
+                <div class="map">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <p>203 Fake St. Mountain View, San Francisco, California, USA</p>
+                </div>
+                <a href=""><i class="fas fa-map-marker-alt"></i> +2 392 3929 210
+</a>
+               <a href=""><i class="fas fa-paper-plane"></i> info@yourdomain.com</a>
+            </div>
+        </div>
+    </footer>
+    
+    <!-- Footer Bottom bar-->
+    <section class="bottomBar">
+        <p>Copyright &copy;2020 All rights reserved | This template is made with <i class="fas fa-heart"></i> by <a href="">Colorlib</a></p>
+        
+    </section> 
+</body>
+</html>
